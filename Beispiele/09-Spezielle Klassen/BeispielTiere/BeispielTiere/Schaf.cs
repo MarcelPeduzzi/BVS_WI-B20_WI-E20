411ab8d34ac2c5ecdfdb878e9356ea9016465d88
Marcel Peduzzi <marcel.peduzzi@hotmail.com>
2022-02-05 10:58:10 +0100

    Beispiel tiere

---
+++ Beispiele/09-Spezielle Klassen/BeispielTiere/BeispielTiere/Schaf.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp11
+{
+    public class Schaf : LandSaeugetier
+    {
+        public Schaf(string name) : base(name)
+        {
+        }
+
+        public void Grasen()
+        {
+            Console.WriteLine("Grasen");
+        }
+
+        public override void PrintTierTyp()
+        {
+            Console.WriteLine("Ich bin ein Schaf");
+        }
+    }
+}
---
+++ Beispiele/09-Spezielle Klassen/BeispielTiere/BeispielTiere/Tiger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp11
+{
+    public class Tiger : LandSaeugetier
+    {
+        public Tiger(string name) : base(name)
+        {
+        }
+
+        public void Jagen()
+        {
+            Console.WriteLine("Jagen");
+        }
+
+        public override void PrintTierTyp()
+        {
+            Console.WriteLine("Ich bin ein Tiger");
+        }
+    }
+}
