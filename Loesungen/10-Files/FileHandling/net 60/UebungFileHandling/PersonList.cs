90ca1bdd7da3960bfac265c5b47a7dfc0ad87449
Marcel Peduzzi <marcel.peduzzi@egeli-informatik.ch>
2022-02-15 20:28:32 +0100

    Folien 11 Interfaces

---
+++ Loesungen/10-Files/FileHandling/net 60/UebungFileHandling/PersonList.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace UebungFileHandling
+{
+    internal class PersonList
+    {
+        // set auf Private damit diese Liste nur innerhabl dieser Klasse
+        // befüllt werden kann
+        public List<Person> personenListe { get; private set; } = new List<Person>();
+
+
+        // overloading FillUp
+        public void FillUp(string filePath)
+        {
+           var lines = File.ReadAllLines(filePath);
+           FillUp(lines);
+        }
+
+        // overloading FillUp
+        public void FillUp(string[] lines)
+        {
+            // Zuerst löschen
+            personenListe.Clear();
+
+            // jetzt abfüllen
+            foreach (string line in lines)
+            {
+                personenListe.Add(new Person(line));
+            }
+        }
+
+        // List auf Console ausgeben
+        public void Print()
+        {
+            foreach (Person person in personenListe)
+            {
+                person.Print();
+            }
+        }
+
+        // Speicher in ein File
+        public void Save(string filePath)
+        {
+            string[] stringArray = new string[personenListe.Count];
+
+            int i = 0;
+            foreach(Person person in personenListe)
+            {
+                stringArray[i++] = person.ToString();
+            }
+            File.WriteAllLines(filePath, stringArray);
+        }
+
+        // Person hinzufügen
+        public void Add(Person person)
+        {
+            personenListe.Add(person);
+        }
+    }
+}
---
+++ Loesungen/10-Files/FileHandling/net 60/UebungFileHandling/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace UebungFileHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // File lesen und auf Console ausgeben
+            string file = @"C:\Temp\Filehandling\Testdatei.txt";
+
+            string[] lines;
+            lines = File.ReadAllLines(file);
+
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            // Erstellen eines Directory
+            Directory.CreateDirectory(@"C:\Temp\Filehandling\TestVerzeichnis");
+
+            // File verschieben
+            // File.Move(@"C:\Temp\Filehandling\Testdatei.txt", @"C:\Temp\Filehandling\TestVerzeichnis\Testdatei.txt");
+
+
+
+            // Zusatz
+            var personList = new PersonList();
+
+            // Liste abfüllen mittels string[]
+            personList.FillUp(lines);
+
+            // List auf Console drucken
+            personList.Print();
+
+            // neue Person der Liste hinzufügen
+            personList.Add(new Person("Knöpfli", "Hansli")
+            {
+                Strasse = "Hüetliweg",
+                HausNr = "12",
+                Plz = "9000",
+                Ort = "St.Gallen"
+            });
+
+            // Liste auf Filesystem speichern
+            personList.Save(@"c:\temp\neu.txt");
+        }
+
+        
+    }
+}
